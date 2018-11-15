79f2a663a9e0405b142f34eeb64cc39c4199ec1d
Chris Wilson <chris@wilson18.com>
2018-11-15 21:39:00 +0000

    Added documentation

--- pyDNS.py
+++ pyDNS.py
@@ -35,6 +35,13 @@ def main():
     exit(EXIT_SUCCESS)
 
 def get_record(dns_handle, fqdn, domain):
+    """
+    Iterates through all DNS records for a domain and returns the record where the data matches the fqdn.
+    :param dns_handle: The pyrax.cloud_dns handle.
+    :param fqdn: The fully qualified domain name to search for.
+    :param domain: Domain to search through.
+    :return: pyrax record for this domain.
+    """
     record = None
     print("Searching for {fqdn}".format(fqdn=fqdn))
     all_records = dns_handle.list_records(domain)
@@ -46,6 +53,13 @@ def get_record(dns_handle, fqdn, domain):
     return record
 
 def create_record(dns_handle, domain, fqdn, ip_addr):
+    """
+    Create a new A record for this fqdn which points to the ip_addr.
+    :param dns_handle: The pyrax.cloud_dns handle.
+    :param domain: The domain to add the entry for.
+    :param fqdn: The fully qualified domain name to add.
+    :param ip_addr: IPv4 Address to save in records data field.
+    """
     record = {
         "type": "A",
         "name": fqdn,
@@ -58,24 +72,41 @@ def create_record(dns_handle, domain, fqdn, ip_addr):
 
 
 def update_record_ip(record, ip):
+    """
+    Calls the record.update function to update the IP address for an existing record.
+    :param record: Record to update.
+    :param ip: The IP to update it to.
+    """
     if ip != record.data:
         print("Updating Record")
         record.update(data=ip)
         print("IP updated to {ip}".format(ip=ip))
     else:
         print("IP Address up to date.")
-    return
 
 
 def get_ipaddr():
+    """
+    Get the current external IP address from ipify.
+    :return: The current IP address.
+    """
     return get('https://api.ipify.org').text
 
 
 def get_hostname():
+    """
+    Get the current systems hostname.
+    :return: The hostname of this machine.
+    """
     return socket.gethostname()
 
 
 def get_file_contents(file_path):
+    """
+    Gets the contents of a file.
+    :param file_path: The path to read the file from.
+    :return: The contents of the file.
+    """
     file_contents = ""
 
     if os.path.exists(file_path):
@@ -86,10 +117,20 @@ def get_file_contents(file_path):
 
 
 def validate_text(text):
+    """
+    Checks if the variable is null or ""
+    :param text:
+    :return: True if valid else false.
+    """
     return text != "" and text != None
 
 
 def parse_config(config_file):
+    """
+    Parses the config file to get the api_key, domain, hostname and usernameusername
+    :param config_file: Path to config file to parse.
+    :return: api_key, domain, hostname, username
+    """
     api_key = ""
     domain = ""
     hostname = ""
@@ -132,6 +173,11 @@ def parse_config(config_file):
 
 
 def setup_api(api_key, username):
+    """
+    Setups up the pyrax API with the information required to connect.
+    :param api_key: API Key to connect with
+    :param username: Username to connect with
+    """
     pyrax.set_setting("identity_type", "rackspace")
     keyring.set_password("pyrax", username, api_key)
     print("Authenticating with {username} using API Key".format(username=username))
